7a771d086297a55143d59ddc88a51a12b6b72b91
Nathan Pertuset <nathanpertuset@gmail.com>
2018-09-13 10:54:35 -0600

    Testing server vars

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,14 @@
 // prod.js - productions keys here
+// module.exports = {
+//   googleClientID: process.env.GOOGLE_CLIENT_ID,
+//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   mongoURI: process.env.MONGO_URI,
+//   cookieKey: process.env.COOKIE_KEY,
+// };
+
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: "142910749179-m7kpptt160tthagioijmhk9kfvm1u4jf.apps.googleusercontent.com",
+  googleClientSecret: "k1DpytQ4OE0CBX58gdWpz-_b",
+  mongoURI: "mongodb://emailyprod:123123A@ds155292.mlab.com:55292/emaily-prod",
+  cookieKey: 'ALSKJFL2KJlksjdflskdjf92'
 };
