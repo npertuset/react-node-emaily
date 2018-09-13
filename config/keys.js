ddb33d5ff356031f222c056279b1bcefde3c6144
Nathan Pertuset <nathanpertuset@gmail.com>
2018-09-13 10:56:08 -0600

    Fixed

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - figure out what set of creentials to return
-if (process.env.NODE_EV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // we are in production - return prod keys
   module.exports = require('./prod');
 } else {
